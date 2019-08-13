625445e2e478d7888f1bf8308f9fedabfc7c34ee
星梦 <hnxsh@foxmail.com>
2019-08-14 00:50:29 +0800

    V2.0.1

--- core/extend/ueditor/ueditor.all.js
+++ core/extend/ueditor/ueditor.all.js
@@ -17564,7 +17564,7 @@ UE.plugins['autofloat'] = function() {
     function updateFloating(){
         var rect3 = getPosition(me.container);
         var offset=me.options.toolbarTopOffset||0;
-        if (rect3.top < 0 && rect3.bottom - toolbarBox.offsetHeight > offset) {
+        if (rect3.top < 0 + topOffset && rect3.bottom - toolbarBox.offsetHeight > offset) {
             setFloating();
         }else{
             unsetFloating();
