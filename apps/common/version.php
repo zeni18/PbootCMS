d780d631d905987e4548657d03fd7a502d0b3de6
星梦 <hnxsh@foxmail.com>
2020-07-06 00:51:04 +0800

    2.1.1

--- apps/common/version.php
+++ apps/common/version.php
@@ -1,12 +1,12 @@
 <?php
 return array(
     // 应用版本
-    'app_version' => '2.1.0',
+    'app_version' => '2.1.1',
     
     // 发布时间
-    'release_time' => '20200621',
+    'release_time' => '20200706',
     
     // 修订版本
-    'revise_version' => '1'
+    'revise_version' => '0'
 
 );
