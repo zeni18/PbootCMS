79f0e905d270d8a55b9b70be5fa4f0c46fd9bc6b
星梦 <hnxsh@foxmail.com>
2021-02-14 00:47:27 +0800

    V3.0.4

--- apps/common/version.php
+++ apps/common/version.php
@@ -2,10 +2,10 @@
 return array(
     // 应用版本
     'app_version' => '3.0.4',
-    
+
     // 发布时间
-    'release_time' => '20201011',
-    
+    'release_time' => '20210214',
+
     // 修订版本
     'revise_version' => '0'
 
